750239dc6d8b8b5edbeb9f0aa0df66c0f714a6ee
Marzuk Islam <marzuk.dev@gmail.com>
2026-07-22 01:13:01 +0600

    calling local llm using python and tokenizer

---
+++ experiments/week01-llm/hello_llm.py
@@ -0,0 +1,19 @@
+from openai import OpenAI
+
+client = OpenAI(
+    base_url="http://127.0.0.1:8080/v1",
+    api_key="none"
+)
+
+response = client.chat.completions.create(
+    model="mlx-community/Qwen3-8B-4bit",
+    messages=[
+        {
+            "role": "user",
+            "content": "Write Hello World in Python"
+        }
+    ]
+)
+
+# print(response)
+print(response.choices[0].message.content)
